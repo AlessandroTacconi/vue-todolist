5cbd885da0e39534cb39e3033fbf253cb9f6b151
AlessandroTacconi <alessandro.tacconi.97@gmail.com>
2023-11-28 16:00:41 +0100

    feat: milestone1

---
+++ main.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// Rifare l'esercizio della to do list.
+// Questa volta però ogni todo sarà un oggetto, formato da due proprietà:
+
+// - text, una stringa che indica il testo del todo
+// - done, un booleano (true/false) che indica se il todo è stato fatto oppure no
+//   MILESTONE 1
+//   Stampare all'interno di una lista HTML un item per ogni todo.
+//   Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      toDos: [
+        {
+          text: 'Giocare a carte',
+          done: true,
+        },
+        {
+          text: 'Guardare il calcio',
+          done: false,
+        },
+      ],
+    };
+  },
+}).mount('#app');
