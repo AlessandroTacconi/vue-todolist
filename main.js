eba8da55595e60a7d5410b73695a457ac98d9254
AlessandroTacconi <alessandro.tacconi.97@gmail.com>
2023-11-28 17:54:54 +0100

    feat: bonus1

--- main.js
+++ main.js
@@ -11,6 +11,8 @@
 // MILESTONE 2
 // Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
 //  Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+//  Bonus:
+//   1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
 
 const { createApp } = Vue;
 
